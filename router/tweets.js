696bc0f607c8d539a8ed0bd668252be061c31d2e
neppiness <0414kjh@naver.com>
2022-04-07 15:10:23 +0900

    Add data validator

--- router/tweets.js
+++ router/tweets.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import {body} from 'express-validator';
+
 import * as tweetController from '../controller/tweet.js';
-import * as tweetRepo from '../data/tweet.js';
+import {validate} from '../middleware/validator.js';
+
+const validator = [
+    body('text').trim().isLength({min: 3}).withMessage('Please enter a message with 3 chars at least.'),
+    // body('name').trim().isLength({min: 2, max: 10}).withMessage('2~10 chars for a name'),
+    // body('username').trim().isLength({min: 2, max: 10}).withMessage('2~10 chars for a username'),
+    validate
+];
 
 const router = express.Router();
 
@@ -11,11 +20,11 @@ router.get('/', tweetController.getAll);
 // GET /tweets/:id
 router.get('/:id', tweetController.getById);
 
-// POST /tweets
-router.post('/', tweetController.post);
+// POST /tweets, with validator
+router.post('/', validator, tweetController.post);
 
-// PUT /tweets/:id
-router.put('/:id', tweetController.putById);
+// PUT /tweets/:id, with validator
+router.put('/:id', validator, tweetController.putById);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.remove);
